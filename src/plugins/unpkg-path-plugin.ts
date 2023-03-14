a6e6d2134cf069631545fd1bf4d84e0c22d4fa85
stgran66 <stanislaw.grygoriew@gmail.com>
2023-03-14 13:47:31 +0100

    added custom caching of packages using indexedDB and localforage lib

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -37,12 +42,25 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+        // Check to see if file has been already fetched, if true it's in cache, return immediatly
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
