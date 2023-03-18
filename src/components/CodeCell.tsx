feec553eba000cf71602c004cfcaf5ef7b11ff10
stgran66 <stanislaw.grygoriew@gmail.com>
2023-03-18 15:30:14 +0100

    added horizontal resizing to code cell, and debounced code bundling into preview iframe to 1000ms

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { bundle } from '../bundler';
 import { CodeEditor } from './CodeEditor';
 import { Preview } from './Preview';
-import { Resizable } from './resizable';
+import { Resizable } from './ResizableBox';
 
 export const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
 
-    setCode(output);
-  };
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue='const a=1;'
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue='const a=1;'
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
