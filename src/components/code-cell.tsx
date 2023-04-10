d9c093bc1d7577290ef6999d8e5ebfb02f322426
stgran66 <stanislaw.grygoriew@gmail.com>
2023-04-10 13:21:26 +0200

    created action bar for moving and deleting cells, also added styling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from 'react';
 import { bundle } from '../bundler';
-import { CodeEditor } from './CodeEditor';
-import { Preview } from './Preview';
-import { Resizable } from './ResizableBox';
+import { CodeEditor } from './code-editor';
+import { Preview } from './code-preview';
+import { Resizable } from './resizable-box';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
-export const CodeCell = () => {
-  const [input, setInput] = useState('');
+interface CodeCellProps {
+  cell: Cell;
+}
+
+export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [err, setErr] = useState('');
   const [code, setCode] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -19,15 +25,15 @@ export const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
           <CodeEditor
-            initialValue='const a=1;'
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} bundlingErr={err} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
