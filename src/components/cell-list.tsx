2d103bdeeeea06450e62ba102737862226ae7fd4
stgran66 <stanislaw.grygoriew@gmail.com>
2023-04-18 15:16:16 +0200

    commented persist middleware, unneccessary for this demo version

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,37 +1,15 @@
 import './cell-list.css';
 import { Fragment } from 'react';
-// import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 import { CellListItem } from './cell-list-item';
 import { AddCell } from './add-cell';
-import { Cell } from '../state';
+// import { Cell } from '../state';
 // import { useActions } from '../hooks/use-actions';
 
-const defaultCells: Cell[] = [
-  {
-    content:
-      '# JS-notebook\n\nThis is an interactive coding environment. You can write JavaScript, see it executed, and write comprehensive documentation using markdown.\n\n- Click any text cell (including this one) to edit it\n- The code in each code editor is all joined together into one file. If you define a variable in cell #1, you can refer to it in any following cell!\n- You can show any React component, string, number, or anything else by calling the `show ` function. This is a function built into this environment. Call show multiple times to show multiple values\n- Re-order or delete cells using buttons on the top right\n- Format code with Prettier using format button\n- Add new cells by hovering on the divider between each cell\n\nAll of your changes get saved to the file you opened JS-notebook with. So if you ran `npx js-notebook-stgran serve test.js`, all of the text and code you write will be saved to the `test.js` file.\n',
-    type: 'text',
-    id: 'djfy9',
-  },
-  {
-    content:
-      "import { useState } from 'react';\n\nconst Counter = () => {\n  const [count, setCount] = useState(0);\n\n  return (\n    <div>\n      <button\n        onClick={() => {\n          setCount(count + 1);\n        }}\n      >\n        Click\n      </button>\n      <h3>Count: {count}</h3>\n    </div>\n  );\n};\n// Display any variable or React Component by calling 'show'\nshow(Counter); //React Component can be displayed as jsx (show(<Counter/>))",
-    type: 'code',
-    id: '2bl4q',
-  },
-  {
-    content:
-      'const App = () => {\n  return (\n    <div>\n      <h3>App says hi!</h3>\n      <i>Counter component will be rendered below...</i>\n      <hr />\n      {/* Counter was declared in the previous cell - \n        we can reference it here! */}\n      <Counter />\n    </div>\n  );\n};\n\nshow(<App/>)',
-    type: 'code',
-    id: '4o15k',
-  },
-];
-
 export const CellList: React.FC = () => {
-  const cells = defaultCells;
-  //   useTypedSelector(({ cells: { order, data } }) =>
-  //   order.map((id) => data[id])
-  // );
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
 
   // const { fetchCells } = useActions();
 
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { persistMiddleware } from './middlewares/persist-middleware';
+// import { persistMiddleware } from './middlewares/persist-middleware';
 
 export const store = createStore(
   reducers,
   {},
-  applyMiddleware(thunk, persistMiddleware)
+  applyMiddleware(
+    thunk
+    // persistMiddleware
+  )
 );
