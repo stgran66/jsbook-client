78c664fa05996aa39d7c70e66969fe3abc6c97d2
stgran66 <stanislaw.grygoriew@gmail.com>
2023-04-18 14:41:28 +0200

    updated project name and homepage

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -3,19 +3,19 @@ import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import { CellListItem } from './cell-list-item';
 import { AddCell } from './add-cell';
-import { useActions } from '../hooks/use-actions';
+// import { useActions } from '../hooks/use-actions';
 
 export const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
 
-  const { fetchCells } = useActions();
+  // const { fetchCells } = useActions();
 
-  useEffect(() => {
-    fetchCells();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   fetchCells();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   const rederedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
